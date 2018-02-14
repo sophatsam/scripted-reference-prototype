21ca1f0204e16efebd0c964ae00c2436dd8dfab4
Sophat Sam <sophat.sam@gmail.com>
2018-02-13 23:58:25 -0500

    add flexbox example

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 (function($){
+
+    // flexbox example
+    $('#flexbox-example-live').on('click', 'li', function() {
+        var value = $(this).text();
+        $('#flexbox-example').css('justify-content', value);
+    })
     
 })(jQuery);
